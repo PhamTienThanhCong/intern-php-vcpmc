f9acedbe982680dbbab9e2344e7e55d39fc284ce
congphamtienthanh <congphamtienthanh@gmail.com>
2023-06-24 21:57:42 +0700

    done Thêm thông tin bản ghi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContractController;
 use App\Http\Controllers\PlayListController;
 use App\Http\Controllers\RecordStoreController;
 use App\Http\Controllers\SchedulePlaylistController;
@@ -55,4 +56,16 @@ Route::resource('schedule-play', SchedulePlaylistController::class)->names([
     'update' => 'SchedulePlay.update',
     'show' => 'SchedulePlay.show',
     'destroy' => 'SchedulePlay.destroy',
+]);
+
+Route::get('/contract-manage/create-done', [ContractController::class, 'createDone'])->name('Manage.Contract.createDone');
+
+Route::resource('contract-manage', ContractController::class)->names([
+    'index' => 'Manage.Contract.index',
+    'create' => 'Manage.Contract.create',
+    'store' => 'Manage.Contract.store',
+    'edit' => 'Manage.Contract.edit',
+    'update' => 'Manage.Contract.update',
+    'show' => 'Manage.Contract.show',
+    'destroy' => 'Manage.Contract.destroy',
 ]);
